0f521a1e0523b82bd8a74cda007d2ed5845ca80f
Caleb Adekoya <caleb123@meta.com>
2024-07-01 17:56:25 -0700

    clean up code

--- src/components/login/SpotifyOAuth.jsx
+++ src/components/login/SpotifyOAuth.jsx
@@ -7,19 +7,6 @@ function SpotifyOAuth(props) {
         window.location.href = `https://accounts.spotify.com/authorize?client_id=${import.meta.env.VITE_SPOTIFY_CLIENT_ID}&redirect_uri=${import.meta.env.VITE_SPOTIFY_REDIRECT_URI}&response_type=code&scope=user-top-read user-read-private`
     }
 
-    // Spotify OAuth - callback function
-    // useEffect(() => {
-    //     const handleSpotifyCallback = async () => {
-    //         const urlParams = new URLSearchParams(window.location.search);
-    //         const code = urlParams.get("code");
-    //         if (code) {
-    //             await API.sendCode(code).then(response => response.json());
-    //             window.location.href = "/l/dashboard";
-    //         }
-    //     }
-    //     handleSpotifyCallback();
-    // }, []);
-
     return (
         <div className="btn btn-sm btn-outline-light" onClick={handleLogin}>
             Login with Spotify
