89a9ca7f14e557438609ac2566b4ff6367f877c0
Caleb Adekoya <caleb123@meta.com>
2024-07-09 10:43:45 -0700

    fetched all artists popularity

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -50,7 +50,7 @@ function Map() {
     }, []);
 
     return (
-        <MapContainer center={[51.505, -0.09]} zoom={2} style={{ height: "100vh", width: "100%" }}>
+        <MapContainer center={[37.8, -96.9]} zoom={5} style={{ height: "100vh", width: "100%" }}>
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
