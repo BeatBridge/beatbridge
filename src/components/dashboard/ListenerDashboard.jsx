a41c26b2b4e2fc22167ad7fc78d80d8b090f8efc
Caleb Adekoya <caleb123@meta.com>
2024-07-12 13:55:57 -0700

    restructured dashboard screen

--- src/components/dashboard/ListenerDashboard.jsx
+++ src/components/dashboard/ListenerDashboard.jsx
@@ -144,6 +144,8 @@ function ListenerDashboard() {
     return (
         <div className='l-dashbaord-container'>
             <div className='row'>
+
+                {/* LEFT COLUMN */}
                 <div className="l-left-sidebar col-md-2">
                     <NavLink to="/" className="l-logo" title='beatbridge_logo'>
                         <div className='parent-logo-container'>
@@ -153,34 +155,8 @@ function ListenerDashboard() {
                             <div>BeatBridge</div>
                         </div>
                     </NavLink>
-                </div>
 
-                <div className='col-md-7'>
-                    <LSearchForm onSearchResults={handleSearchResults} onSuggestionClick={handleSuggestionClick} />
-                </div>
-
-                <div className='l-right-sidebar col-md-3'>
-                    <div className='l-right-sidebar-user-info'>
-                        <div className='l-dashboard-profile-container'>
-                            <FaUser className='l-dash-user-icon' />
-                        </div>
-
-                        <div className='l-dashboard-user-name'>
-                            <h4>Welcome, {userInfo.username}</h4>
-                            <p>{userInfo.isPremium ? 'Premium' : 'Free'} Subscriber</p>
-                            <SpotifyOAuth isSpotifySignedIn={isSpotifySignedIn} />
-                            <button onClick={handleLogout}>Logout</button>
-                        </div>
-                    </div>
-                    <div className='l-bell-icon-container'>
-                        <FaBell className='l-dash-bell-icon' />
-                    </div>
-                </div>
-            </div>
-
-            <main>
-                <div className='row l-main-top'>
-                    <div className='col-md-2 l-menu-bar'>
+                    <div className='menu-items-container'>
                         <h4>Menu</h4>
                         <hr />
 
@@ -206,9 +182,35 @@ function ListenerDashboard() {
                                 <NavLink to='/trending'><h5>Trending</h5></NavLink>
                             </div>
                         </div>
-                    </div>
 
-                    <div className='col-md-7 l-discover-genre'>
+                    </div>
+                    <div>
+                        <h4>Help</h4>
+                        <hr />
+
+                        <div className='l-menu-items'>
+                            <div className='l-dashbaord-menu-items'>
+                                <FontAwesomeIcon icon={faGear} className='menu-icon' />
+                                <NavLink to='/settings'><h5>Settings</h5></NavLink>
+                            </div>
+                            <div className='l-dashbaord-menu-items'>
+                                <FontAwesomeIcon icon={faMicrochip} className='menu-icon' />
+                                <NavLink to='/chatbot'><h5>Chat With AI</h5></NavLink>
+                            </div>
+                            <div className='l-dashbaord-menu-items'>
+                                <FontAwesomeIcon icon={faEarthAmericas} className='menu-icon' />
+                                <NavLink to='/map'><h5>Map</h5></NavLink>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* MIDDLE COLUMN */}
+                <div className='col-md-7 scrollable-column'>
+                    <div>
+                        <LSearchForm onSearchResults={handleSearchResults} onSuggestionClick={handleSuggestionClick} />
+                    </div>
+                    <div>
                         <div className='l-discover-genre-top'>
                             <h5>
                                 <FaMusic className='l-discover-genre-music-icon' />
@@ -246,44 +248,7 @@ function ListenerDashboard() {
                             />
                         </div>
                     </div>
-
-                    <div className='col-md-3 l-right-menu'>
-                        <h4 className='l-top-artist-column'>Global Top 50</h4>
-                        {isSpotifySignedIn ? (
-                            globalTop50.length !== undefined ? (
-                                <div>
-                                    <GlobalTop50 tracks={globalTop50.slice(0, 3)} onTrackClick={handleTrackClick} />
-                                </div>
-                            ) : (
-                                <p>No top 50 artists found.</p>
-                            )
-                        ) : (
-                            <p>Please sign in to Spotify to view this data.</p>
-                        )}
-                    </div>
-                </div>
-                <div className='row l-main-bottom'>
-                    <div className='col-md-2 l-menu-bar-bottom'>
-                        <h4>Help</h4>
-                        <hr />
-
-                        <div className='l-menu-items'>
-                            <div className='l-dashbaord-menu-items'>
-                                <FontAwesomeIcon icon={faGear} className='menu-icon' />
-                                <NavLink to='/settings'><h5>Settings</h5></NavLink>
-                            </div>
-                            <div className='l-dashbaord-menu-items'>
-                                <FontAwesomeIcon icon={faMicrochip} className='menu-icon' />
-                                <NavLink to='/chatbot'><h5>Chat With AI</h5></NavLink>
-                            </div>
-                            <div className='l-dashbaord-menu-items'>
-                                <FontAwesomeIcon icon={faEarthAmericas} className='menu-icon' />
-                                <NavLink to='/map'><h5>Map</h5></NavLink>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className='col-md-7 l-top-music'>
+                    <div>
                         <div className='l-top-music-top'>
                             <h5>
                                 <FontAwesomeIcon icon={faHeadphonesSimple} className='l-top-music-icon-header' />
@@ -307,8 +272,44 @@ function ListenerDashboard() {
                             )}
                         </div>
                     </div>
+                </div>
 
-                    <div className='col-md-3 l-track-action'>
+                {/* RIGHT COLUMN */}
+                <div className='l-right-sidebar col-md-3'>
+                    <div className='l-right-sidebar-top'>
+                        <div className='l-right-sidebar-user-info'>
+                            <div className='l-dashboard-profile-container'>
+                                <FaUser className='l-dash-user-icon' />
+                            </div>
+
+                            <div className='l-dashboard-user-name'>
+                                <h4>Welcome, {userInfo.username}</h4>
+                                <p>{userInfo.isPremium ? 'Premium' : 'Free'} Subscriber</p>
+                                <SpotifyOAuth isSpotifySignedIn={isSpotifySignedIn} />
+                                <button onClick={handleLogout}>Logout</button>
+                            </div>
+                        </div>
+                        <div className='l-bell-icon-container'>
+                            <FaBell className='l-dash-bell-icon' />
+                        </div>
+                    </div>
+
+                    <div>
+                        <h4 className='l-top-artist-column'>Global Top 50</h4>
+                        {isSpotifySignedIn ? (
+                            globalTop50.length !== undefined ? (
+                                <div>
+                                    <GlobalTop50 tracks={globalTop50.slice(0, 3)} onTrackClick={handleTrackClick} />
+                                </div>
+                            ) : (
+                                <p>No top 50 artists found.</p>
+                            )
+                        ) : (
+                            <p>Please sign in to Spotify to view this data.</p>
+                        )}
+                    </div>
+
+                    <div>
                         {selectedTrack && (
                             <div className='selected-track-details'>
                                 <h4>{selectedTrack.name}</h4>
@@ -343,7 +344,7 @@ function ListenerDashboard() {
                         )}
                     </div>
                 </div>
-            </main>
+            </div>
         </div>
     );
 }
