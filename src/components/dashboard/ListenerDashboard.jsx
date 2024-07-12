67853da54845f65e34e7979bba7c6483af518cae
Caleb Adekoya <caleb123@meta.com>
2024-07-12 11:03:25 -0700

    clean up code

--- src/components/dashboard/ListenerDashboard.jsx
+++ src/components/dashboard/ListenerDashboard.jsx
@@ -101,7 +101,6 @@ function ListenerDashboard() {
         };
         setSelectedTrack(updatedTrack);
         setShowTaggingForm(false);
-        trackArtistSearch(suggestion.artists[0].id);
 
         if (track.artists && track.artists.length > 0) {
             trackArtistSearch(track.artists[0].id);
